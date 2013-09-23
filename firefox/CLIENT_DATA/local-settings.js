9a155b760b0440daaaa4f14e9f50cad02b0b7894
tobkern <tobkern1980@googlemail.com>
2013-09-23 09:19:19 +0200

    Create local-settings.js

---
+++ firefox/CLIENT_DATA/local-settings.js
@@ -0,0 +1,8 @@
+pref("general.config.obscure_value", 0);
+pref("general.config.filename", "mozilla.cfg");
+// The PAC file to load.  Ignored unless network.proxy.type is 2.
+pref("network.proxy.autoconfig_url", "http://MyPage/autoproxy.pac");
+pref("network.proxy.type",2); //manueller proxy
+//pref("network.proxy.http", "backspace.MyPage.de");
+//pref("network.proxy.http_port", 3128);
+//pref("network.proxy.no_proxies_on", "localhost,127.0.0.1,192.168.0.0/16")
