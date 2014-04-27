cc927d41b46495e2bf5478a44682c2874a50384a
tobkern <tobkern1980@googlemail.com>
2014-04-27 04:53:42 +0200

    Create post_install.sh

---
+++ tools/post_install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# Script-Name : $@.sh
+# Version : 0.01
+# Autor : $USER
+# Datum : $DATE
+# Lizenz : GPLv3
+# Depends : samba samba-tools, 
+# Use :
+#
+# Example:
+#
+# Description:
+###########################################################################################
+## 
+##
+###########################################################################################
+#
+# add user for Domain Join
+# it will be save so use joinuser !
+smbpasswd -a root
+
+##### not finished #######!!!!!!!!!!!!!!!!!!!
