0476f78940ce4801df34131d9236e3ad6b3a3630
tobkern <tobkern1980@googlemail.com>
2014-12-19 09:54:57 +0100

    Update getdownloadlink.sh

--- getdownloadlink.sh
+++ getdownloadlink.sh
@@ -1,19 +1,33 @@
 #!/bin/bash -x
-
+#
+# Script-Name : getdownloadlink.sh
+# Version : 0.01
+# Autor : Tobias Kern
+# Datum : 10.02.2011
+# Lizenz : GPLv3
+# Depends : none
+# Use : 
+#
+# Example:
+#
+# Description:
+###########################################################################################
+## Some Info and so one.
+##
+###########################################################################################
 LANG=de_DE.UTF-8
 LANGUAGE=de_DE.UTF-8
 
 OS="win"
 LANG="de"
 
-
 # Erzeugt den Aktuellen link zum Download mit wget
-getUrl ()
+_getUrl ()
 {
 LINK="$(lynx -source -assume_charset=utf8 -preparsed http://www.mozilla.org/en-US/firefox/all/|grep "os=$OS"|grep "lang=$LANG"|grep "product=firefox-*"| sed 's/'HREF'/'LINKFIREFOX'/g' | sed 's/'amp\;'/''/g' )"
 echo $LINK
 }
 
-getUrl > downlink.link
+_getUrl > downlink.link
 
 exit 0
